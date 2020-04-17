bb0696914b55549252e12abbb453fd5e5814df34
Hampus <hampus.lundblad@hotmail.se>
2020-04-17 20:24:43 +0200

    fixade knapparna på startsidan, igen

--- src/components/lindButtonArrow.js
+++ src/components/lindButtonArrow.js
@@ -5,8 +5,8 @@ import { NavLink } from "react-router-dom";
 const Styles = styled.div`
   .btn {
     width: 20rem;
-    padding:1rem;
-    margin-top:1rem;
+    padding: 1rem;
+    margin-top: 1rem;
     background-color: #2c5075;
     border-radius: 0px;
     color: white;
@@ -16,32 +16,25 @@ const Styles = styled.div`
       background-color: #112c48;
     }
   }
-  .Nav__link{
-
+  .Nav__link {
     color: white;
     font-family: "PT Serif", serif;
     text-align: left;
     text-decoration: none;
-    }
-  `;
+  }
+`;
 
 function LindButtonArrow(props) {
   return (
     <Styles>
-      <div className="btn d-flex flew-row justify-content-start">
-          <NavLink
-            className="Nav__link col-md-11"
-            activeClassName="active"
-            exact
-            to="/about"
-          >
-            {props.btntext}
-          </NavLink>
-       
-        <div className="col-md-1">
-          <i className="fas fa-chevron-right"></i>
+      <NavLink className="Nav__link" activeClassName="active" exact to="/about">
+        <div className="btn d-flex flew-row justify-content-start">
+          <div className="col-md-11">{props.btntext}</div>
+          <div className="col-md-1">
+            <i className="fas fa-chevron-right"></i>
+          </div>
         </div>
-      </div>
+      </NavLink>
     </Styles>
   );
 }
